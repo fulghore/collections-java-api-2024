109bb93a3766639be665da6f6e775d196c540a9a
fulghore <aldrin.bl17@gmail.com>
2024-08-28 01:58:18 -0300

    Atualização exercício 1

---
+++ collections-java-api-2024/src/list/operacaoes_basicas/ListaTarefas.java
@@ -0,0 +1,35 @@
+package list.operacaoes_basicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    //atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    //metodos
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    };
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) ;
+            tarefasParaRemover.add(t);
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public String obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+}
---
+++ collections-java-api-2024/src/list/operacaoes_basicas/Tarefa.java
@@ -0,0 +1,14 @@
+package list.operacaoes_basicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
